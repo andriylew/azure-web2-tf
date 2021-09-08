c44b47c5b7b27fb198e25d03706b64d13a2eb6e0
Ubuntu <ubuntu@ip-172-31-8-201.eu-north-1.compute.internal>
2021-09-08 22:01:00 +0000

    first commit

---
+++ app_serv.tf
@@ -0,0 +1,85 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tf-rg-group" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_app_service_plan" "tf-service-plan" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.tf-rg-group.location
+  resource_group_name = azurerm_resource_group.tf-rg-group.name
+  kind = "Linux"
+  reserved = true 
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+  tags = {
+    environment = "dev"
+  }
+
+}
+
+resource "azurerm_app_service" "tf-app-service" {
+  name                = var.app_service_name
+  location            = azurerm_resource_group.tf-rg-group.location
+  resource_group_name = azurerm_resource_group.tf-rg-group.name
+  app_service_plan_id = azurerm_app_service_plan.tf-service-plan.id
+
+  site_config {
+    linux_fx_version = "PHP|7.4"
+    scm_type         = "LocalGit"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+
+}
+
+
+resource "azurerm_mysql_server" "tf-mysql-serv" {
+  name                = var.sql_server_name
+  location            = azurerm_resource_group.tf-rg-group.location
+  resource_group_name = azurerm_resource_group.tf-rg-group.name
+
+  administrator_login          = "alev-admin"
+  administrator_login_password = "HelloPassword1"
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "5.7"
+  auto_grow_enabled                 = false
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = false
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = false
+}
+
+resource "azurerm_mysql_database" "tf-mysql-db" {
+  name                = "tf-web2-mysql-db"
+  resource_group_name = azurerm_resource_group.tf-rg-group.name
+  server_name         = azurerm_mysql_server.tf-mysql-serv.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
+
+resource "azurerm_mysql_firewall_rule" "allow_azure_services" {
+  name                = "office"
+  resource_group_name = azurerm_resource_group.tf-rg-group.name
+  server_name         = azurerm_mysql_server.tf-mysql-serv.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
+
+
+output "mysql_host" {
+  value = azurerm_mysql_server.tf-mysql-serv.fqdn
+
+}
+
+
---
+++ pipeln.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    azuredevops = {
+      source = "microsoft/azuredevops"
+      version = ">=0.1.0"
+    }
+  }
+}
+
+provider "azuredevops" {
+  org_service_url       = "https://dev.azure.com/andriylew29"
+  personal_access_token = var.personalacctoken
+#set with env var as AZDO_PERSONAL_ACCESS_TOKEN
+
+}
+
+resource "azuredevops_project" "project" {
+  name            = "alev-tf-pipeline"
+  visibility      = "private"
+  version_control = "Git"
+  features = {
+    "boards"       = "enabled"
+    "repositories" = "enabled"
+    "pipelines"    = "enabled"
+    "testplans"    = "enabled"
+    "artifacts"    = "enabled"
+  }
+}
+
+resource "azuredevops_serviceendpoint_github" "serviceendpoint_alev_gh" {
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "Sample GithHub Personal Access Token"
+
+  auth_personal {
+    # personal_access_token set with AZDO_GITHUB_SERVICE_CONNECTION_PAT environment variable
+    
+  }
+}
+
+resource "azuredevops_build_definition" "build" {
+  project_id = azuredevops_project.project.id
+  name       = "Sample Build Definition"
+
+
+  ci_trigger {
+    use_yaml = true
+  }
+
+  repository {
+    repo_type             = "GitHub"
+    repo_id               = "andriylew/azure-web2-priv"
+    branch_name           = "main"
+    yml_path              = "azure-pipelines.yml"
+    service_connection_id = azuredevops_serviceendpoint_github.serviceendpoint_alev_gh.id
+  }
+
+}
+resource "azuredevops_serviceendpoint_azurerm" "endpointazure" {
+  project_id                = azuredevops_project.project.id
+  service_endpoint_name     = "owner-alev-terraform"
+  description               = "Managed by Terraform"
+  azurerm_subscription_name = "ownserv"
+}
+
+output "azureSubscription" {
+  value = azuredevops_serviceendpoint_azurerm.endpointazure.id
+
+}
+
+
